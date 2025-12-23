933a1a01e51ebaa5dabce099b7b8f97deaa33d28
NickEngle <nick.w.engle@gmail.com>
2025-12-23 10:09:48 -0500

    update app version to 1.0.7

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import { useAuth } from "./lib/auth";
 import { ActiveAthleteProvider } from "./context/ActiveAthleteContext";
 
 // App version - keep in sync with main.tsx
-export const APP_VERSION = '1.0.6';
+export const APP_VERSION = '1.0.7';
 
 export default function App() {
   const { user, initializing, signingInWithLink } = useAuth();
